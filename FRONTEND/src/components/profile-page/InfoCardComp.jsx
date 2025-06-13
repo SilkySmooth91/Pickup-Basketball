aa78d6e119d3b14a72c7226c909d35ccc6852c83
SilkySmooth91 <lorenzo.olivieri13@gmail.com>
2025-06-13 17:56:13 +0200

    UX modali

--- FRONTEND/src/components/profile-page/InfoCardComp.jsx
+++ FRONTEND/src/components/profile-page/InfoCardComp.jsx
@@ -133,10 +133,10 @@ export default function InfoCardComp({ profile, isOwner }) {
       {/* MODALE CAMBIO PASSWORD */}
       {showModal && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60">
-          <div className="bg-white rounded-lg shadow-lg p-8 sm:w-11/12 md:w-full max-w-md relative">
+          <div className="bg-white rounded-lg shadow-lg p-8 md:mt-10 sm:w-11/12 md:w-full max-w-md relative">
             <button className="absolute top-2 right-2 text-gray-400 hover:text-gray-700 text-2xl" onClick={() => setShowModal(false)} aria-label="Chiudi">×</button>
             <h2 className="text-xl font-bold mb-4 text-orange-600">Cambia password</h2>
-            <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4 !mb-2">
               <FloatingLabel 
                 id="old-password" 
                 type="password" 
